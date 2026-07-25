ff627aaa3af01dc0b2beff077431767285e89e89
sayghteight <sayghteight@gmail.com>
2026-07-25 16:39:06 +0200

    feat: implement automatic application updates with Tauri Updater

--- src/App.tsx
+++ src/App.tsx
@@ -14,6 +14,7 @@ import { UniversePanel } from './features/project/UniversePanel';
 import { WordImportPanel } from './features/import/WordImportPanel';
 import { TimelinePanel } from './features/project/TimelinePanel';
 import { CalendarsPanel } from './features/project/CalendarsPanel';
+import { UpdateDialog } from './components/UpdateDialog';
 
 const App: React.FC = () => {
   const { isOpen } = useProjectStore();
